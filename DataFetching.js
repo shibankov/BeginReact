f570798e7f38d011cdf9a2410a5021f2b6bf4930
shibankov <47152751+shibankov@users.noreply.github.com>
2021-07-22 13:22:39 +0300

    Update DataFetching.js
    update

--- DataFetching.js
+++ DataFetching.js
@@ -4,45 +4,46 @@ import ReactPaginate from 'react-paginate'
 
 function DataFetching() {
   const [items,setItems] = useState([]);
+  const [nav,setNav] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(null);
-
   const [currentPage,setCurrentPage] = useState(1);
 
-  const PER_PAGE = 10;//Кол-во елементов на каждой страницы
+  useEffect(() =>{
+    fetch('https://pickcase.ua/bitrix/services/main/ajax.php?action=generator&model=iphone-x&page=2&c=diva:rest&mode=ajax&lang=ru?action=generator&model=iphone-x&c=diva:rest&mode=ajax&lang=ru')
+      .then((res) => res.json())
+      .then(
+        (res) =>{
+          setIsLoaded(true);
+          setItems(res.data.list);
+          setNav(res.data.nav);
+        },
+        (error) =>{
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  },[]);
+
+  // console.log(currentPage)
+  const PER_PAGE = 4;//Кол-во елементов на каждой страницы
   const offset = currentPage * PER_PAGE;//Кол-во елементов который были отображены на предыдущих страницах
 
   const currentPageData = items
     .slice(offset, offset + PER_PAGE)
-    .map(item =>(
-      <li key={item.id} className="card-list__item">
-        <h3>{item.title}</h3>
-        <p>{item.body}</p>
-      </li>
-    ));
+    .map((item) =>
+      <div key={item.id} className="card-list__item">
+        <img  src={item.img.[0].url} alt={item.name} loading="lazy"/>
+        <h3 >{item.name}</h3>
+      </div>
+    );
 
   const pageCount = Math.ceil(items.length / PER_PAGE);
+
 
   function handlePageClick({ selected: selectedPage }) {
     setCurrentPage(selectedPage);
   }
-
-  useEffect(() =>{
-    fetch('https://pickcase.ua/bitrix/services/main/ajax.php?action=generator&model=iphone-x&page=2&c=diva:rest&mode=ajax&lang=ru?action=generator&model=iphone-x&c=diva:rest&mode=ajax&lang=ru ')
-      .then((res) => res.json())
-      .then(
-      (res) =>{
-        setIsLoaded(true);
-        setItems(res);
-        // console.log(res)
-
-      },
-      (error) =>{
-        setIsLoaded(true);
-        setError(error);
-      }
-      )
-    },[]);
 
     if (error) {
       return <div>Ошибка: {error.message}</div>;
